7b582906ca8b6c03614736b37e6c5b3e18d54aab
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-01 12:09:47 +0000

    Remove .gitignore

---
+++ client/build/precache-manifest.33dc5d4ce0c9925bb54bc60878e1323d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21cdf54b55032a87e97c58df89f31859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b2a1e69d0c39e5f954",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "364026bb88ecafc1602c",
+    "url": "/static/js/2.6d5c0e83.chunk.js"
+  },
+  {
+    "revision": "c2b2a1e69d0c39e5f954",
+    "url": "/static/js/main.d62b22c7.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
